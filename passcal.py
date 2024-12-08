e50507838b41867c0447803851606cbc9b9dd350
Aiden Szeto <66970460+aidenszeto@users.noreply.github.com>
2024-12-08 15:00:20 -0800

    Update passcal.py

--- passcal.py
+++ passcal.py
@@ -3,6 +3,7 @@ import requests
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2 import service_account
 import datetime
 import pickle
 
